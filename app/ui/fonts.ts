1c359318960cdebfdcac02974378d8d88ce66cdb
Mauro Mendez <mauromm1996@gmail.com>
2024-06-25 14:13:21 +0400

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Questrial, Lusitana } from 'next/font/google';
+
+
+export const questrial = Questrial({ weight: ['400'], subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+
